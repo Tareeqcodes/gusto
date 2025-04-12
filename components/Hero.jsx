47f9d9a6e33437c932048546ded70f47645be767
Devtareeq <101841038+Tareeqcodes@users.noreply.github.com>
2025-04-12 09:04:35 +0100

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -66,7 +66,7 @@ export default function Hero() {
           src="/images/hero.png"
           fill
           sizes="100vw"
-          priority={false}
+          loading='lazy'
           className="object-cover"
           alt="Gusto Restaurant Interior"
           quality={100}
